5e0d05fff8dca840d5326382a9b4b87d6b65068d
gnatb <90341198+gnatb@users.noreply.github.com>
2022-06-13 12:52:45 -0500

    Base

---
+++ morse_led.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+
+from gpiozero import LED
+from time import sleep
+from signal import pause
+
+led = LED(25)
+
+
+def shortSig():
+    print('short')
+    led.on ()
+    sleep (0.5)
+    led.off()
+    sleep(0.5)
+    
+def longSig ():
+    print('long')
+    led.on ()
+    sleep (1.5)
+    led.off()
+    sleep(0.5)
+
+sosMsg = input ('Enter your message:')
+print(sosMsg)
+sosLen = len(sosMsg)
+print (sosLen)
+
+
+
+while True:
+
+    counter = 0
+    
+    while counter < sosLen:
+        sosLetter = sosMsg[counter]
+        sleep(3)
+    
+        print (sosLetter)
+    
+        if (sosLetter == 'a'):
+            shortSig()
+            longSig()
+        elif (sosLetter == 'b') :
+            longSig ()
+            shortSig()
+            shortSig()
+            shortSig()
+        elif (sosLetter == 'c'):
+            longSig()
+            shortSig()
+            longSig()
+            shortSig()
+        elif (sosLetter== 'd'):
+            longSig()
+            shortSig()
+            shortSig()
+        else:
+            print ('done')
+    
+        counter = counter + 1
+
+
+
